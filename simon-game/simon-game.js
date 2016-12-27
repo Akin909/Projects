d1640b5c5302c7cc27103f3a74005bce6f2fbfa9
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-27 13:03:16 +0000

    add wrapper function to prevent autostart

--- simon-game/simon-game.js
+++ simon-game/simon-game.js
@@ -1,75 +1,88 @@
 //Is for commented out code
 /* Is for commenting code in general */
 
-const quadrants = document.querySelectorAll('.quadrants');
-const start = document.querySelector('.controls')
-quadrants.forEach((quadrant)=>addEventListener('click',glow))
 const startBtn = document.querySelector('.start');
-const stopBtn = document.querySelector('.stop');
-const scoreOutput = document.querySelector('.score')
-const playerOrder = [];
-const compOrder = [];
-let counter = 0;
-let counterLimit = 3
-let score = 0;
+  const quadrants = document.querySelectorAll('.quadrants');
+  const stopBtn = document.querySelector('.stop');
+  const scoreOutput = document.querySelector('.score')
+  const playerOrder = [];
+  const compOrder = [];
+  let counter = 0;
+  let counterLimit = 1
+  let score = 0;
+
+function start(){
 
 
 
-function glow(event){
+  quadrants.forEach((quadrant)=>addEventListener('click',glow))
 
-  let quadrant = event.target;
-  playerOrder.push(quadrant)
-  console.log("player",playerOrder,"player length",length)
-  quadrant.classList.add("active");
-  setTimeout(()=>quadrant.classList.remove("active"),400);
-  /*Right length is declared here as it must be within this functions scope to register changes an compare properly*/
-  let rigthLength = (playerOrder.length === compOrder.length);
+  function glow(event){
 
-  /*Compares player input to computer order then adds score and increases counter;*/
+    let quadrant = event.target;
+    playerOrder.push(quadrant)
+    console.log("player",playerOrder,"player length",length)
+    quadrant.classList.add("active");
+    setTimeout(()=>quadrant.classList.remove("active"),400);
 
-  let correctAnswer = playerOrder.every((element,i)=>{
-    if(rigthLength){
-      return element === compOrder[i];
+    /*Right length is declared here as it must be within this functions scope to register changes an compare properly*/
+    let rigthLength = (playerOrder.length === compOrder.length);
+
+    /*Compares player input to computer order then adds score and increases counter;*/
+
+    let correctAnswer = playerOrder.every((element,i)=>{
+      if(rigthLength){
+        return element === compOrder[i];
+      }
+    });
+
+    if(correctAnswer){
+        score += 1;
+        scoreOutput.innerHTML  = score;
+        counter = 0;
+        counterLimit +=1;
+      }
+      else if(rigthLength && !correctAnswer){
+          scoreOutput.innerHTML = "!!";
+          function replaySequence(quadrant){
+            console.log("quadrant",quadrant)
+            if(compOrder.indexOf(quadrant)===-1){
+              compOrder.forEach((quadrant)=>glow())
+            }
+          }
+        replaySequence(quadrant)
     }
-  });
-
-  if(correctAnswer){
-      // alert("EQUALS");
-      score += 1;
-      scoreOutput.innerHTML  = score;
-      counter = 0;
-      counterLimit +=1;
-    } else if(rigthLength && !correctAnswer){
-      scoreOutput.innerHTML = "!!"
-  }
-}
-
-function glowRandom(){
-  let i = getRandomArbitrary();
-  counter++;
-  console.log(counter)
-  /*counter stops the function once it has run a certain number of times*/
-  if(counter<=counterLimit){
-    quadrants[i].classList.add("active");
-    compOrder.push(quadrants[i]);
-    console.log("computer",compOrder,"length",compOrder.length)
-    setTimeout(()=>quadrants[i].classList.remove("active"),600);
-  }
-  // else{
-  //   clearInterval(sequence)
-  // }
-}
-
-function getRandomArbitrary(){
-  return Math.round(Math.random()*3);
-}
-/*Sets the interval has global scope so sets onload, is the only way that it can be cleared*/
-let sequence = setInterval(glowRandom,1500);
-function setSequence(event){
-  if(event.target === stopBtn){
-    clearInterval(sequence);
   }
 
+  function glowRandom(){
+    let i = getRandomArbitrary();
+    counter++;
+    console.log(counter)
+    /*counter stops the function once it has run a certain number of times*/
+    if(counter<=counterLimit){
+      quadrants[i].classList.add("active");
+      compOrder.push(quadrants[i]);
+      console.log("computer",compOrder,"length",compOrder.length)
+      setTimeout(()=>quadrants[i].classList.remove("active"),600);
+    }
+    else{
+      // counter+=0
+      /*clearInterval(sequence)*/
+    }
+  }
+
+  function getRandomArbitrary(){
+    return Math.round(Math.random()*3);
+  }
+  /*Sets the interval has global scope so sets onload, is the only way that it can be cleared*/
+  let sequence = setInterval(glowRandom,1500);
+  function setSequence(event){
+    if(event.target === stopBtn){
+      clearInterval(sequence);
+    }
+  }
+  stopBtn.addEventListener('click',setSequence)
 }
-stopBtn.addEventListener('click',setSequence)
-startBtn.addEventListener('click',setSequence)
+
+
+startBtn.addEventListener('click',start)
