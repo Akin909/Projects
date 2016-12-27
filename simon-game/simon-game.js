9c6825dd4ebd411a59155f6855f7fd3a5f673c99
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-27 00:44:22 +0000

    add buttons and starting and stopping feature

--- simon-game/simon-game.js
+++ simon-game/simon-game.js
@@ -1,28 +1,40 @@
 const quadrants = document.querySelectorAll('.quadrants');
 const start = document.querySelector('.controls')
 quadrants.forEach((quadrant)=>addEventListener('click',glow))
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
+const playerOrder = [];
+const compOrder = [];
+
 
 function glow(event){
   let quadrant = event.target;
+  playerOrder.push(quadrant)
+  console.log(playerOrder)
   quadrant.classList.add("active");
-
   setTimeout(()=>quadrant.classList.remove("active"),400);
 }
 
 function glowRandom(){
   let i = getRandomArbitrary();
-  quadrants[i].classList.add("active");
-  setTimeout(()=>quadrants[i].classList.remove("active"),400);
+  let counter=0;
+  counter += 1
+  // while(counter<3){
+  // quadrants[i].classList.add("active");
+  // compOrder.push(quadrants[i]);
+  // console.log(compOrder)
+  // setTimeout(()=>quadrants[i].classList.remove("active"),400);
+  // }
+  return;
 }
 function getRandomArbitrary(){
   return Math.round(Math.random()*3);
 }
 
 function setSequence(){
-
-  let sequence = setInterval(glowRandom,2000);
-  sequence();
-  setTimeout(clearInterval(sequence),3000)
+  // debugger;
+let sequence = setInterval(()=>glowRandom(),1500);
 
 }
-setSequence()
+// setTimeout(clearInterval(sequence),3000)
+startBtn.addEventListener('click',setSequence)
