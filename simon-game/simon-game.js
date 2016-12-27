3b6286ae685fcfbe3d8616ae8e41c0933df560b2
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-27 11:38:37 +0000

    add output screen for score plus initial functionality for longer running game with increasing difficulty

--- simon-game/simon-game.js
+++ simon-game/simon-game.js
@@ -3,31 +3,49 @@ const start = document.querySelector('.controls')
 quadrants.forEach((quadrant)=>addEventListener('click',glow))
 const startBtn = document.querySelector('.start');
 const stopBtn = document.querySelector('.stop');
+const scoreOutput = document.querySelector('.score')
 const playerOrder = [];
 const compOrder = [];
-
+let counter = 0;
+let counterLimit = 3
+let score = 0;
 
 function glow(event){
   let quadrant = event.target;
   playerOrder.push(quadrant)
-  // console.log(playerOrder)
+  console.log("player",playerOrder)
   quadrant.classList.add("active");
   setTimeout(()=>quadrant.classList.remove("active"),400);
+  //Compares player input to computer order then ... do something
+  if(playerOrder.every((element,i)=>{
+    if(playerOrder.length===compOrder.length){
+    return element === compOrder[i];
+      }
+    })){
+      // alert("EQUALS");
+      score += 1;
+      scoreOutput.innerHTML  = score;
+      counter = 0;
+      counterLimit +=1;
+    };
 }
-let counter = 0;
+
 function glowRandom(){
   let i = getRandomArbitrary();
   counter++;
   console.log(counter)
   //counter stops the function once it has run a certain number of times
-  if(counter<=3){
-  quadrants[i].classList.add("active");
-  compOrder.push(quadrants[i]);
-  // console.log(compOrder)
-  setTimeout(()=>quadrants[i].classList.remove("active"),600);
+  if(counter<=counterLimit){
+    quadrants[i].classList.add("active");
+    compOrder.push(quadrants[i]);
+    console.log("computer",compOrder)
+    setTimeout(()=>quadrants[i].classList.remove("active"),600);
+  }
+  // else{
+  //   clearInterval(sequence)
+  // }
 }
 
-}
 function getRandomArbitrary(){
   return Math.round(Math.random()*3);
 }
@@ -40,7 +58,5 @@ function setSequence(event){
   }
 
 }
-// ()=>clearInterval(sequence)
-//setTimeout(clearInterval(sequence),3000)
 stopBtn.addEventListener('click',setSequence)
 startBtn.addEventListener('click',setSequence)
