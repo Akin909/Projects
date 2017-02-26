725729b1c144dd9a689fa901ed430809e111111d
Akin909 <akin.sowemimo@gmail.com>
2017-02-26 14:30:39 +0000

    add best moves
    use minmax algorithim to calculate best move for AI and worst move for human player given current state of the game

--- Tic-Tac-toe/tic-tac-toe.js
+++ Tic-Tac-toe/tic-tac-toe.js
@@ -37,22 +37,76 @@ function winning(board, player) {
 
 // Checks for the terminal  states such as win, lose, and tie and returning
 // a value accordingly
-if (winning(newBoard,huPlayer)) {
-	return {score:-10};
-} else if (winning(newBoard,aiPlayer)){
-	return {score:10};
+if (winning(newBoard, huPlayer)) {
+	return {
+		score: -10
+	};
+} else if (winning(newBoard, aiPlayer)) {
+	return {
+		score: 10
+	};
 } else if (availSpots.length === 0) {
-	return {score:0};
+	return {
+		score: 0
+	};
 }
 
 
 var moves = []
-forEach(function(square,index){
-    return ( typeof square === 'string' )
+	//Move through the available spots
+origBoard.forEach(function(square, index) {
+	// Create an object for each and store the index of that spot
+	var move = {};
+	move.index = newBoard[availSpots[index]];
+
+	// set the empty spot to current player
+	newBoard[availSpots[index]] = player;
+	/*collect the score resulted from calling minimax on the opponent of the
+		* current player */
+
+	if (player == aiPlayer) {
+		var result = minimax(newBoard,huPlayer)	
+		move.score = result.score;
+	} else {
+		var result = minimax(newBoard,aiPlayer);
 	
+	}
+	// reset the spot to empty
+	newBoard[availSpots[index]] = move.index;
+
+	//push the object to the array
+	move.push(moves);
+
+
 });
 
+// if it is the computer's turn loop over the moves and choose the move with
+// the highest score
+var bestMove;
 
+if (player === aiPlayer) {
+	var bestScore = -1000;	
+	for (var i = 0, len = moves.length; i < len; i++) {
+		if (moves[i].score > bestScore) {
+			bestScore = moves[i].score;
+			bestMove = i;
+		}	
+	}
+} else {
+	// else loop over the moves and choose the move with the lowest score
+	var bestScore = 10000;
+	for (var i = 0, len = moves.length; i < len; i++) {
+		if (moves[i].score < bestScore){
+			bestScore = moves[i].score;
+			bestMove = i
+		}
+	}
+
+
+	// Return the chosen move (object) from the moves array
+		return moves[bestMove];
+
+}
 
 
 
